b7b9ceb3376d5a136fc63aa95562ed721313c999
huyluvnhjm99 <datpro7703@gmail.com>
2021-06-28 13:56:27 +0700

    Update ui turnoffdevice screen
    Update ui turnoffdevice screen

--- flutter_drownsi/lib/ui/turnOnDevice_screen.dart
+++ flutter_drownsi/lib/ui/turnOnDevice_screen.dart
@@ -18,38 +18,66 @@ class TurnOnOffDevice extends StatelessWidget{
         child: Padding(
           padding: const EdgeInsets.only(top: 50),
           child: SizedBox(
-            width: 200,
+            width: 250,
             child: Column(
               children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 30.0,),
+                    Text(
+                      'Control device',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    const SizedBox(width: 10.0,),
+                    Image(
+                      image: AssetImage("assets/images/qr_icon.png"),
+                      width: 50.0,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0,),
                 FlatButton(
                   height: 100,
                   onPressed: ()=>{
-                  qrData = "turn on device",
+                  qrData = "command=turnon",
                   showDialog(context: context, builder: (context){
                     return Dialog(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                       elevation: 16,
                       child: Container(
-                        height: 300,
-                        width: 260,
+                        height: 400,
+                        width: 300,
                         child: Column(
                           children: [
                             QrImage(data: qrData, padding: const EdgeInsets.only(top: 20,left: 20,right: 20,bottom: 20)),
+                            RichText(
+                              text: TextSpan(
+                                  children: <TextSpan>[
+                                    TextSpan(text: 'Scan QR code to ', style: TextStyle(color: Colors.black)),
+                                    TextSpan(text: 'turn on', style: TextStyle(color: Colors.green)),
+                                    TextSpan(text: ' detect feature', style: TextStyle(color: Colors.black)),
+                                  ]
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            Image(
+                              image: AssetImage("assets/images/qr_icon.png"),
+                              width: 50.0,
+                            ),
                           ],
                         ),
                       ),
                     );
                   })
                 },
-                  color: HexColor("#2ad921"),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image(
-                        image: AssetImage("assets/images/qr_icon.png"),
-                        width: 50.0,
+                        image: AssetImage("assets/images/turn on.png"),
+                        width: 200.0,
                       ),
-                      Text('Turn on device',textAlign: TextAlign.center, style: TextStyle(color: Colors.white),)
+
                     ],
                   ),
                 ),
@@ -57,29 +85,44 @@ class TurnOnOffDevice extends StatelessWidget{
                 FlatButton(
                   height: 100,
                   onPressed: ()=>{
-                  qrData = "turn off device",
+                  qrData = "command=turnoff",
                   showDialog(context: context, builder: (context){
                     return Dialog(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
                       elevation: 16,
                       child: Container(
-                        height: 300,
-                        width: 260,
+                        height: 400,
+                        width: 300,
                         child: Column(
                           children: [
                             QrImage(data: qrData, padding: const EdgeInsets.only(top: 20,left: 20,right: 20,bottom: 20)),
+                            RichText(
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    TextSpan(text: 'Scan QR code to ', style: TextStyle(color: Colors.black)),
+                                    TextSpan(text: 'turn off', style: TextStyle(color: Colors.red)),
+                                    TextSpan(text: ' detect feature', style: TextStyle(color: Colors.black)),
+                                  ]
+                                ),
+                            ),
+                            SizedBox(height: 5,),
+                            Image(
+                              image: AssetImage("assets/images/qr_icon.png"),
+                              width: 50.0,
+                            ),
                           ],
                         ),
                       ),
                     );
                   })
                 },
-                  color: HexColor("#d94621"),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.subdirectory_arrow_left, color: Colors.white,),
-                      Text('Turn off device',textAlign: TextAlign.center, style: TextStyle(color: Colors.white),)
+                      Image(
+                        image: AssetImage("assets/images/turn off.png"),
+                        width: 200.0,
+                      ),
                     ],
                   ),
                 ),
